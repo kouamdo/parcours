a6c3c9fa2a880762f63a5b2bab116ed9dc9cd8aa
adeline <adeline.ngo@simofintech.com>
2022-10-13 11:06:16 +0100

    Dans cette branche se trouveront toutes les version du travail en cours. Pour le momment c'est ici que ce trouve l'implementation du service translate. En temps normal les fichiers modifies devraient apparaitre dans la merge request mais au cas, je les cite. app.modules.ts : ici j'ai definie les paramettres du translateModule qui se charge de recuperer les traductions dans les fichiers de langue, app.component.ts : ici j'implemente le constructeur en lui disant de d'utiliser l'anglais en tant que langue par defaut (ceci prend effet si un des fichers de langues n'arrive a se charger correctement), et une fonction useLanguage pour permettre a l'utilisateur de choisir lui-meme une langue. tsconfig.json : ici j'ai rajouter deux variables pour autoriser importation des fichiers json. app.component.html : ici j'ai remplacer certains textes du Menu gauche par les variables correspondantes dans les json de langues. J'ai inclus les objets avec leurs textes dans tous les fichiers de langue avec les memes valeurs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import defaultLanguage from './../assets/i18n/en.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
+}) 
 export class AppComponent {
   title = 'Parcours client - clinique de ...';
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PatientsModule } from './modules/patients/patients.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemDBService } from './backend/backend';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { TranslateModule, TranslateLoader, TranslateCompiler } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateMessageFormatCompiler } from 'ngx-translate-messageformat-compiler';
 
 @NgModule({
   declarations: [
@@ -18,12 +21,33 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule,
     FormsModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemDBService),
-    PatientsModule
+    PatientsModule,
+
+    // ngx-translate and the loader module
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        },
+
+        // compiler configuration
+        compiler: {
+            provide: TranslateCompiler,
+            useClass: TranslateMessageFormatCompiler
+        }
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
