27e1b1e342ae100dcbd15ab2fb280e9235bb287b
adeline <adeline.ngo@simofintech.com>
2022-11-18 12:12:13 +0100

    erreurs non resolues

--- src/app/modules/services/list-services/list-services.component.ts
+++ src/app/modules/services/list-services/list-services.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { EMPTY, Observable } from 'rxjs';
 import { IService } from 'src/app/modele/service';
 import { ServicesService } from 'src/app/services/services/services.service';
@@ -7,13 +8,13 @@ import { ServicesService } from 'src/app/services/services/services.service';
 @Component({
   selector: 'app-list-services',
   templateUrl: './list-services.component.html',
-  styleUrls: ['./list-services.component.css']
+  styleUrls: ['./list-services.component.scss']
 })
 export class ListServicesComponent implements OnInit {
 
   services$:Observable<IService[]>=EMPTY;
 
-  constructor(private router:Router, private serviceService:ServicesService) { }
+  constructor(private translate: TranslateService, private router:Router, private serviceService:ServicesService) { }
 
   ngOnInit(): void {
     this.services$ = this.getAllServices();
--- src/app/modules/services/services.module.ts
+++ src/app/modules/services/services.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ServicesRoutingModule } from './services-routing.module';
 import { ListServicesComponent } from './list-services/list-services.component';
 import { NewServicesComponent } from './new-services/new-services.component';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { HttpLoaderFactory } from 'src/app/app.module';
 
 
 @NgModule({
@@ -13,7 +19,19 @@ import { NewServicesComponent } from './new-services/new-services.component';
   ],
   imports: [
     CommonModule,
-    ServicesRoutingModule
+    ServicesRoutingModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    TranslateModule.forChild({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        },
+        extend:true
+    }),
+    BrowserModule 
   ]
 })
 export class ServicesModule { }
