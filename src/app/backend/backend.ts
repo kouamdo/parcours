40e9fcbf6fbfd5dc086f476ac220462122aa76e2
adeline <adeline.ngo@simofintech.com>
2023-01-27 11:16:28 +0100

    update

--- src/app/backend/backend.ts
+++ src/app/backend/backend.ts
@@ -22,16 +22,16 @@ export class InMemDBService implements InMemoryDbService{
             {id:"3", libelle:"Consultation", etat:"non attribue",dateDerniereModification: "07/03/1990",dateAttribution: "07/03/1990",dateFin: "07/03/1990",nombreTotalAttributions: 50}
         ];
         let tickets:ITicket[]=[
-            {id:1, idUnique:"20221206S1A01", date_heure: new Date("01/13/2022"), idFileAttente: "S1A01", idPersonne: "1", statut: this.statutTicketTraite},
-            {id:2, idUnique:"20221206S1A02", date_heure: new Date("01/12/2021"), idFileAttente: "S1A01", idPersonne: "2", statut: this.statutTicketActif},
-            {id:3, idUnique:"20221206S1A01", date_heure: new Date("01/11/2020"), idFileAttente: "S1A02", idPersonne: "1", statut: this.statutTicketActif},
-            {id:4, idUnique:"20221206S1A02", date_heure: new Date("01/14/2023"), idFileAttente: "S1A02", idPersonne: "2", statut: this.statutTicketTraite},
-            {id:5, idUnique:"20221206S1A03", date_heure: new Date("01/11/2019"), idFileAttente: "S1A03", idPersonne: "1", statut: this.statutTicketActif},
-            {id:6, idUnique:"20221206S1A04", date_heure: new Date("01/14/2018"), idFileAttente: "S1A04", idPersonne: "2", statut: this.statutTicketActif},
-            {id:7, idUnique:"20221206S1A03", date_heure: new Date("01/11/2018"), idFileAttente: "S1A03", idPersonne: "1", statut: this.statutTicketActif},
-            {id:8, idUnique:"20221206S1A04", date_heure: new Date("01/14/2017"), idFileAttente: "S1A04", idPersonne: "2", statut: this.statutTicketActif},
-            {id:9, idUnique:"20221206S1A05", date_heure: new Date("01/11/2017"), idFileAttente: "S1A05", idPersonne: "1", statut: this.statutTicketActif},
-            {id:10, idUnique:"20221206S1A06", date_heure: new Date("01/14/2016"), idFileAttente: "S1A06", idPersonne: "2", statut: this.statutTicketActif}
+            {id:"1", idUnique:"20221206S1A01", date_heure: new Date("01/13/2022"), idFileAttente: "S1A01", idPersonne: "1", statut: this.statutTicketTraite},
+            {id:"2", idUnique:"20221206S1A02", date_heure: new Date("01/12/2021"), idFileAttente: "S1A01", idPersonne: "2", statut: this.statutTicketActif},
+            {id:"3", idUnique:"20221206S1A01", date_heure: new Date("01/11/2020"), idFileAttente: "S1A02", idPersonne: "1", statut: this.statutTicketActif},
+            {id:"4", idUnique:"20221206S1A02", date_heure: new Date("01/14/2023"), idFileAttente: "S1A02", idPersonne: "2", statut: this.statutTicketTraite},
+            {id:"5", idUnique:"20221206S1A03", date_heure: new Date("01/11/2019"), idFileAttente: "S1A03", idPersonne: "1", statut: this.statutTicketActif},
+            {id:"6", idUnique:"20221206S1A04", date_heure: new Date("01/14/2018"), idFileAttente: "S1A04", idPersonne: "2", statut: this.statutTicketActif},
+            {id:"7", idUnique:"20221206S1A03", date_heure: new Date("01/11/2018"), idFileAttente: "S1A03", idPersonne: "1", statut: this.statutTicketActif},
+            {id:"8", idUnique:"20221206S1A04", date_heure: new Date("01/14/2017"), idFileAttente: "S1A04", idPersonne: "2", statut: this.statutTicketActif},
+            {id:"9", idUnique:"20221206S1A05", date_heure: new Date("01/11/2017"), idFileAttente: "S1A05", idPersonne: "1", statut: this.statutTicketActif},
+            {id:"10", idUnique:"20221206S1A06", date_heure: new Date("01/14/2016"), idFileAttente: "S1A06", idPersonne: "2", statut: this.statutTicketActif}
         ];
         let menus:IMenus[] =[ 
           {"idUser":"phil", "langue":"fr","fonctionnalites":[
