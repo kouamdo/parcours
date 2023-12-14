f74255bbba4498057dafe67d5b915fc97dd62160
Adeline13 <88739706+Adeline13@users.noreply.github.com>
2023-12-14 11:43:45 +0100

    Develop extend sous document (#65)
    * integration des sous documents, ok.
    
    * correction de merge conflict.
    
    * correction merge conflicts avec develop
    
    * Develop extend sous document (#60) (#63)
    
    * integration des sous documents, ok.
    
    * correction de merge conflict.
    
    * correction merge conflicts avec develop

--- src/app/services/donnees-echange/donnees-echange.service.ts
+++ src/app/services/donnees-echange/donnees-echange.service.ts
@@ -7,6 +7,7 @@ export class DonneesEchangeService {
   dataDocumentCategorie : any;
   dataDocumentPrecoMvts : any;
   dataDocumentAttributs : any;
+  dataDocumentSousDocuments : any;
   dataEnteteMenu : any;
   dataDocumentSousDocuments : any;
   constructor() { }
